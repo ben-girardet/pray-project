bee132496b1ccaa1326a14988fa8cb1f6939efe4
Ben Girardet <ben@platform5.ch>
2020-11-21 17:45:35 +0100

    display redis host:port in redis client start

--- server/src/core/redis.ts
+++ server/src/core/redis.ts
@@ -1,9 +1,14 @@
 import redis from 'redis';
 import { promisify } from 'util';
 
+const host = process.env.REDIS_HOST as string ?? '127.0.0.1';
+const port = parseInt(process.env.REDIS_PORT as string) ?? 6379;
+console.log('redis host:', host);
+console.log('redis port:', port);
+
 const client = redis.createClient({
-    port: parseInt(process.env.REDIS_PORT as string) ?? 6379,
-    host: process.env.REDIS_HOST as string ?? '127.0.0.1'
+    port,
+    host
 });
 
 export const getAsync = promisify(client.get).bind(client);
