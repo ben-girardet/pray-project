531324aea54bc0af42fc664ed73b16f1768a9378
Ben Girardet <ben@platform5.ch>
2020-11-25 14:03:21 +0100

    sameSite = false based on origin

--- server/src/resolvers/auth.ts
+++ server/src/resolvers/auth.ts
@@ -24,7 +24,10 @@ export class AuthResolver {
         }
         const refreshTokenData = user.generateRefreshToken();
         await user.save();
-        this.sendRefreshToken(context.res, refreshTokenData, !context.req.originalUrl.includes('localhost'));
+        const origin = context.req.get('origin') || '';
+        console.log('origin', origin);
+        console.log('origin.includes(localhost)', origin.includes('localhost'));
+        this.sendRefreshToken(context.res, refreshTokenData, !origin.includes('localhost'));
         const jwtString = jwt.sign({userId: user.id, roles: user.roles}, process.env.JWT_SECRET_OR_KEY as string, { expiresIn: process.env.JWT_TOKEN_EXPIRATION, algorithm: 'HS256' });
         // this.setJWTCookie(context.res, jwtString);
         const login = new Login();
@@ -47,7 +50,8 @@ export class AuthResolver {
         if (!foundUser) throw new Error('Invalid refresh token');
         const refreshTokenData = foundUser.generateRefreshToken();
         await foundUser.save();
-        this.sendRefreshToken(context.res, refreshTokenData, !context.req.originalUrl.includes('localhost'));
+        const origin = context.req.get('origin') || '';
+        this.sendRefreshToken(context.res, refreshTokenData, !origin.includes('localhost'));
         const jwtString = jwt.sign({userId: foundUser.id, roles: foundUser.roles}, process.env.JWT_SECRET_OR_KEY as string, { expiresIn: process.env.JWT_TOKEN_EXPIRATION, algorithm: 'HS256'});
         // this.setJWTCookie(context.res, jwtString);
         const login = new Login();
@@ -59,6 +63,7 @@ export class AuthResolver {
     }
 
     private sendRefreshToken(res: Response, refreshTokenData: RefreshTokenData, sameSite = true) {
+        console.log('sameSite', sameSite);
         res.cookie('refreshToken', refreshTokenData.refreshToken, {
             path: '/graphql',
             httpOnly: true,
