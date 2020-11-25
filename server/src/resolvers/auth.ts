8b1d7342792e29261749f8920602a533383d4ee8
Ben Girardet <ben@platform5.ch>
2020-11-25 14:10:15 +0100

    try secure: !sameSite

--- server/src/resolvers/auth.ts
+++ server/src/resolvers/auth.ts
@@ -70,6 +70,7 @@ export class AuthResolver {
             expires: moment(refreshTokenData.expiry).add(1, 'hour').toDate(),
             domain: undefined,
             //domain: 'localhost',
+            secure: !sameSite,
             sameSite: sameSite || 'none'
         });
     }
