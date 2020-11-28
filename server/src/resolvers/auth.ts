c600dc746cb9bf96283c0c52efd4627656d4de70
Ben Girardet <ben@platform5.ch>
2020-11-28 17:35:41 +0100

    fix origin not defined

--- server/src/resolvers/auth.ts
+++ server/src/resolvers/auth.ts
@@ -49,6 +49,7 @@ export class AuthResolver {
         if (!foundUser) throw new Error('Invalid refresh token');
         const refreshTokenData = foundUser.generateRefreshToken();
         await foundUser.save();
+        const origin = context.req.get('origin') || '';
         const sameSite = context.req.hostname === 'localhost' || !origin.includes('localhost');
         this.sendRefreshToken(context.res, refreshTokenData, sameSite);
         const jwtString = jwt.sign({userId: foundUser.id, roles: foundUser.roles}, process.env.JWT_SECRET_OR_KEY as string, { expiresIn: process.env.JWT_TOKEN_EXPIRATION, algorithm: 'HS256'});
