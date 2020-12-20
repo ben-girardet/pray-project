e996d6ef599df4cc1050674d858cc2e639afc7f6
Ben Girardet <ben@platform5.ch>
2020-12-20 21:36:18 +0100

    pm2 start -f

--- client/start-prod.sh
+++ client/start-prod.sh
@@ -1,3 +1,1 @@
-kill $(lsof -t -i:8080)
-pm2 start /home/jelastic/ROOT/client/node_modules/http-server/bin/http-server --name client -- /home/jelastic/ROOT/client/build  -p 8080 -d false -i false
-# npx http-server build -d 0 -i 0
+pm2 start /home/jelastic/ROOT/client/node_modules/http-server/bin/http-server --name client -f -- /home/jelastic/ROOT/client/build  -p 8080 -d false -i false
