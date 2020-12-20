ac5f73adebf2a8f59c8f2974d1cdd851058e0ab8
Ben Girardet <ben@platform5.ch>
2020-12-20 21:25:15 +0100

    fix builds

--- client/start-prod.sh
+++ client/start-prod.sh
@@ -1,3 +1,3 @@
 kill $(lsof -t -i:8080)
-pm2 start /home/jelastic/ROOT/client/node_modules/http-server/binhttp-server --name client -- /home/jelastic/ROOT/client/build  -p 8080 -d false -i false
+pm2 start /home/jelastic/ROOT/client/node_modules/http-server/bin/http-server --name client -- /home/jelastic/ROOT/client/build  -p 8080 -d false -i false
 # npx http-server build -d 0 -i 0
--- server/start-prod.sh
+++ server/start-prod.sh
@@ -1,3 +1,3 @@
 kill $(lsof -t -i:3000)
 # npx nodemon build/server/src/server.js
-pm2 --name api build/server/src/server.js
+pm2 start --name api build/server/src/server.js
