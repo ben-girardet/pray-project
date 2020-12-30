b8ba2ddbb3ff3f2730d8a2e37db0eb5ccf277bb2
Ben Girardet <ben@platform5.ch>
2020-12-30 14:37:32 +0100

    try to fix the same site issue

--- server/src/resolvers/auth.ts
+++ server/src/resolvers/auth.ts
@@ -29,7 +29,8 @@ export class AuthResolver {
         const refreshTokenData = user.generateRefreshToken();
         await user.save();
         const origin = context.req.get('origin') || '';
-        const sameSite = context.req.hostname === 'localhost' || !origin.includes('localhost');
+        const sameSite = !context.req.hostname.includes('https://env-4211245.jcloud-ver-jpc.ik-server.com')
+            && (context.req.hostname === 'localhost' || !origin.includes('localhost'));
         this.sendRefreshToken(context.res, refreshTokenData, sameSite);
         const jwtString = jwt.sign({userId: user.id, roles: user.roles}, process.env.JWT_SECRET_OR_KEY as string, { expiresIn: process.env.JWT_TOKEN_EXPIRATION, algorithm: 'HS256' });
         // this.setJWTCookie(context.res, jwtString);
@@ -57,7 +58,8 @@ export class AuthResolver {
         const refreshTokenData = foundUser.generateRefreshToken();
         await foundUser.save();
         const origin = context.req.get('origin') || '';
-        const sameSite = context.req.hostname === 'localhost' || !origin.includes('localhost');
+        const sameSite = !context.req.hostname.includes('https://env-4211245.jcloud-ver-jpc.ik-server.com')
+            && (context.req.hostname === 'localhost' || !origin.includes('localhost'));
         this.sendRefreshToken(context.res, refreshTokenData, sameSite);
         const jwtString = jwt.sign({userId: foundUser.id, roles: foundUser.roles}, process.env.JWT_SECRET_OR_KEY as string, { expiresIn: process.env.JWT_TOKEN_EXPIRATION, algorithm: 'HS256'});
         // this.setJWTCookie(context.res, jwtString);
