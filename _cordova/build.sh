333b0a8ff7d562fabac19e3f84e33363243538b8
Ben Girardet <ben@platform5.ch>
2021-01-04 20:14:01 +0100

    improve cordova tooling

--- _cordova/build.sh
+++ _cordova/build.sh
@@ -1,9 +1,69 @@
 rm -rf www/*
 cp -R ../client/dist/* www
-cp resources/cordova.css www/cordova.css
-sed -i'.orig' -e 's+</body>+<script src="cordova.js"></script></body>+g' www/index.html
+cp resources/app-cordova.css www/app-cordova.css
+cp resources/app-cordova.js www/app-cordova.js
+sed -i'.orig' -e 's+</body>+<script src="app-cordova.js"></script><script src="cordova.js"></script></body>+g' www/index.html
 rm www/index.html.orig
 sed -i'.orig' -e 's+<body>+<body class="cordova">+g' www/index.html
 rm www/index.html.orig
-sed -i'.orig' -e 's+</head>+<link rel="stylesheet" type="text/css" href="cordova.css"></head>+g' www/index.html
+sed -i'.orig' -e 's+</head>+<link rel="stylesheet" type="text/css" href="app-cordova.css"></head>+g' www/index.html
 rm www/index.html.orig
+
+# read -r VERSION<../_cordova/version.json
+VERSION=`cat ../_cordova/version.json`
+echo "$VERSION"
+MAJOR=$(jq '.major' <<< "$VERSION")
+MINOR=$(jq '.minor' <<< "$VERSION")
+PATCH=$(jq '.patch' <<< "$VERSION")
+BUILD=$(jq '.build' <<< "$VERSION")
+
+echo "Major: $MAJOR"
+echo "Minor: $MINOR"
+echo "Path: $PATCH"
+echo "Build: $BUILD"
+
+NEWBUILD=$(($BUILD + 1))
+echo "New build: $NEWBUILD"
+
+# TODO: ensure that no value (MAJOR, MINOR, PATH OR BUILD) is larger
+# than 99. Ideally the script should update the previous value when larger
+# than 99
+# BUILD value can go up to 999
+
+echo "### Important ###"
+echo "Make sur that no version number is larger than 99 !"
+
+NEWVERSION=$(jq '.build += 1' <<< "$VERSION") && \
+echo "${NEWVERSION}" > ../_cordova/version.json
+
+VERSIONSTRING="$MAJOR.$MINOR.$PATCH"
+IOSBDLVERSION="$NEWBUILD"
+ANDROIDVERSION="$(($MAJOR*1000000+$MINOR*100000+$PATCH*1000+$NEWBUILD))"
+
+echo "VERSIONSTRING: $VERSIONSTRING";
+echo "IOSBDLVERSION: $IOSBDLVERSION";
+echo "ANDROIDVERSION: $ANDROIDVERSION";
+
+searchVS='(version=")([0-9.]*)(")'
+replaceVS="\1${VERSIONSTRING}\3"
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  sed -i "" -E "s/${searchVS}/${replaceVS}/g" "config.xml"
+else
+  sed -i -E "s/${searchVS}/${replaceVS}/g" "config.xml"
+fi
+
+searchIOS='(ios-CFBundleVersion=")([0-9.]*)(")'
+replaceIOS="\1${IOSBDLVERSION}\3"
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  sed -i "" -E "s/${searchIOS}/${replaceIOS}/g" "config.xml"
+else
+  sed -i -E "s/${searchIOS}/${replaceIOS}/g" "config.xml"
+fi
+
+searchAND='(android-versionCode=")([0-9.]*)(")'
+replaceAND="\1${ANDROIDVERSION}\3"
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  sed -i "" -E "s/${searchAND}/${replaceAND}/g" "config.xml"
+else
+  sed -i -E "s/${searchAND}/${replaceAND}/g" "config.xml"
+fi
---
+++ _cordova/resources/app-cordova.js
@@ -0,0 +1,2 @@
+console.log('window.fetch', window.fetch);
+console.log('window.cordovaFetch', window.cordovaFetch);
