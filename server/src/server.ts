4d76e70351ac3b1e3df68b780a2b0202cfbc10be
Ben Girardet <ben@platform5.ch>
2021-01-29 18:29:28 +0100

    diminish the amount of sentry transactions

--- server/src/server.ts
+++ server/src/server.ts
@@ -127,23 +127,21 @@ mongoose.connect(
         environment: process.env.SENTRY_ENV || 'unset',
         dsn: process.env.SENTRY_DSN,
         debug: true,
-        integrations: [
-            // enable HTTP calls tracing
-            new Sentry.Integrations.Http({ tracing: true }),
-            // enable Express.js middleware tracing
-            new Tracing.Integrations.Express({
-              // to trace all requests to the default router
-              app,
-              // alternatively, you can specify the routes you want to trace:
-              // router: someRouter,
-            }),
-          ],
-        // We recommend adjusting this value in production, or using tracesSampler
-        // for finer control
-        tracesSampleRate: 1.0,
+                    // integrations: [
+                    //     // enable HTTP calls tracing
+                    //     new Sentry.Integrations.Http({ tracing: true }),
+                    //     // enable Express.js middleware tracing
+                    //     new Tracing.Integrations.Express({
+                    //     // to trace all requests to the default router
+                    //     app,
+                    //     // alternatively, you can specify the routes you want to trace:
+                    //     // router: someRouter,
+                    //     }),
+                    // ],
+                    // // We recommend adjusting this value in production, or using tracesSampler
+                    // // for finer control
+                    // tracesSampleRate: 1.0,
       });
-
-
 
     const server = http.createServer(app);
     const io = socket(server);
@@ -151,8 +149,8 @@ mongoose.connect(
 
     app.use(filter());
 
-    app.use(Sentry.Handlers.requestHandler());
-    app.use(Sentry.Handlers.tracingHandler());
+    // app.use(Sentry.Handlers.requestHandler());
+    // app.use(Sentry.Handlers.tracingHandler());
 
     app.use(helmet({
         contentSecurityPolicy: {
